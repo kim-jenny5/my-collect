1e1492db0f1e585d2bd3b05f6e2c9f95f9f4d714
Jenny Kim <kimj1905@gmail.com>
2021-03-04 15:15:49 -0500

    commit tree eba83872e86395239ac9a8b2fe9cf6f5641ee3d8

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,9 @@
-
-
+def my_collect(collection)
+    i = 0
+    diff_collection = []
+    while i < collection.size
+        diff_collection << yield(collection[i])
+        i += 1
+    end
+    diff_collection
+end
